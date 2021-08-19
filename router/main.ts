3e4601ba29daf8791bab058802f25d79024e41fd
Jiseok Oh <jiseok95@gmail.com>
2021-08-19 16:07:14 +0900

    Add: CRUD

--- router/main.ts
+++ router/main.ts
@@ -4,77 +4,227 @@ import fs from "fs";
 export default (app: express.Express) => {
   app.use(express.json());
 
-  app.get("/", (request, response) => {
+  app.get("/", (_request, response) => {
     response.render("index", {
       title: "MY HP",
       length: 5,
     });
   });
 
-  app.get("/list", (request, response) => {
-    fs.readFile(__dirname + "/../data/" + "user.json", "utf-8", (err, data) => {
-      console.log(err ?? "WORKING");
-      console.log(data);
-      response.send(data);
-    });
+  app.get("/list", (_request, response) => {
+    fs.readFile(
+      __dirname + "/../data/" + "user.json",
+      "utf-8",
+      (read_error, data) => {
+        console.log(read_error ?? "WORKING");
+
+        if (read_error) {
+          response.json(resultFailure("Read File Error Occurred"));
+          return;
+        }
+
+        console.log(data);
+        response.json(resultSuccess({ data: JSON.parse(data) }));
+        return;
+      },
+    );
   });
 
   app.get("/getUser", (request, response) => {
-    fs.readFile(__dirname + "/../data/" + "user.json", "utf-8", (err, data) => {
-      console.log(err ?? "WORKING");
-      const parsed: UserData = JSON.parse(data);
-      const target_user = parsed.users.find(
-        ({ name }) => name === request.query.name,
-      );
-      response.json(target_user);
-    });
+    console.log(request.query);
+    if (typeof request.query.name !== "string") {
+      response.json(resultFailure("Invalid Request"));
+      return;
+    }
+
+    fs.readFile(
+      __dirname + "/../data/" + "user.json",
+      "utf-8",
+      (read_error, data) => {
+        console.log(read_error ?? "WORKING");
+        if (read_error) {
+          response.json(resultFailure("Read File Error Occurred"));
+          return;
+        }
+
+        const parsed: UserData = JSON.parse(data);
+        const target_user = parsed.users.find(
+          ({ name }) => name === request.query.name,
+        );
+
+        if (target_user == undefined) {
+          response.json(resultFailure("Can Not Find User Name"));
+          return;
+        }
+
+        response.json(resultSuccess({ data: target_user }));
+        return;
+      },
+    );
   });
 
   app.post("/addUser", (request: Request<User>, response) => {
     console.log(JSON.stringify(request.body));
-    if (!(request.body.name && request.body.password)) {
-      response.json({
-        success: 0,
-        error: "Invalid Request.",
-      });
+    if (isUser(request.body) === false) {
+      response.json(resultFailure("Invalid Request"));
       return;
     }
 
-    fs.readFile(__dirname + "/../data/" + "user.json", "utf-8", (err, data) => {
-      if (err) {
-        response.send("Read File Error");
-        return;
-      }
-      const parsed: UserData = JSON.parse(data);
-      if (parsed.users.findIndex((x) => x.name == request.body.name) !== -1) {
-        response.json({
-          success: 0,
-          error: "Duplicate Name.",
-        });
-        return;
-      }
-      const to_write: UserData = { users: [...parsed.users, request.body] };
+    fs.readFile(
+      __dirname + "/../data/" + "user.json",
+      "utf-8",
+      (read_error, data) => {
+        if (read_error) {
+          response.json(resultFailure("Read File Error Occurred"));
+          return;
+        }
 
-      fs.writeFile(
-        __dirname + "/../data/" + "user.json",
-        JSON.stringify(to_write, null, "\t"),
-        (err) => {
-          if (err) {
-            response.send("Write File Error");
+        const parsed: UserData = JSON.parse(data);
+
+        if (parsed.users.findIndex((x) => x.name == request.body.name) !== -1) {
+          response.json(resultFailure("Duplicate Name"));
+          return;
+        }
+
+        const to_write: UserData = { users: [...parsed.users, request.body] };
+
+        fs.writeFile(
+          __dirname + "/../data/" + "user.json",
+          JSON.stringify(to_write, null, "\t"),
+          (write_error) => {
+            if (write_error) {
+              response.send("Write File Error");
+              return;
+            }
+            response.json(resultSuccess());
             return;
-          }
-          response.json({ success: 1 });
-        },
-      );
-    });
+          },
+        );
+      },
+    );
+  });
+
+  app.put("/updateUser", (request: Request<UserUpdateData>, response) => {
+    if (
+      !(
+        request.body.from &&
+        request.body.to &&
+        isUser(request.body.from) &&
+        isUser(request.body.to)
+      )
+    ) {
+      response.json(resultFailure("Invalid Request"));
+      return;
+    }
+
+    fs.readFile(
+      __dirname + "/../data/" + "user.json",
+      "utf-8",
+      (read_error, data) => {
+        if (read_error) {
+          response.json(resultFailure("Read File Error Occurred"));
+          return;
+        }
+
+        const parsed: UserData = JSON.parse(data);
+        const target_index = parsed.users.findIndex(
+          (x) => x.name == request.body.from.name,
+        );
+
+        if (target_index === -1) {
+          response.json(resultFailure("Can Not Find User Name"));
+          return;
+        }
+
+        const to_write: UserData = {
+          users: [
+            ...parsed.users.slice(0, target_index),
+            ...parsed.users.slice(target_index + 1),
+            request.body.to,
+          ],
+        };
+
+        fs.writeFile(
+          __dirname + "/../data/" + "user.json",
+          JSON.stringify(to_write, null, "\t"),
+          (write_error) => {
+            if (write_error) {
+              response.send("Write File Error");
+              return;
+            }
+            response.json(resultSuccess());
+            return;
+          },
+        );
+      },
+    );
+  });
+
+  app.delete("/deleteUser/:username", (request, response) => {
+    const target_name = request.params.username;
+    fs.readFile(
+      __dirname + "/../data/" + "user.json",
+      "utf-8",
+      (read_error, data) => {
+        if (read_error) {
+          response.json(resultFailure("Read File Error Occurred"));
+          return;
+        }
+
+        const parsed: UserData = JSON.parse(data);
+        const target_index = parsed.users.findIndex(
+          (x) => x.name == target_name,
+        );
+
+        if (target_index === -1) {
+          response.json(resultFailure("Can Not Find User Name"));
+          return;
+        }
+
+        const to_write: UserData = {
+          users: [
+            ...parsed.users.slice(0, target_index),
+            ...parsed.users.slice(target_index + 1),
+          ],
+        };
+
+        fs.writeFile(
+          __dirname + "/../data/" + "user.json",
+          JSON.stringify(to_write, null, "\t"),
+          (write_error) => {
+            if (write_error) {
+              response.send("Write File Error");
+              return;
+            }
+            response.json(resultSuccess());
+            return;
+          },
+        );
+      },
+    );
   });
 };
+
+function isUser(mayBeUser: any | User): mayBeUser is User {
+  return mayBeUser.name && mayBeUser.password;
+}
+
+function resultFailure(error_message: string): FailureResult {
+  return { success: 0, error_message };
+}
+
+function resultSuccess(data?: Object): SuccessResult {
+  return { success: 1, ...data };
+}
 
 interface User {
   name: string;
   password: string;
 }
-
+interface UserUpdateData {
+  from: User;
+  to: User;
+}
 interface UserData {
   users: User[];
 }
